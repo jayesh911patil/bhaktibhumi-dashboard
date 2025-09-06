6562a66d2ce9332d9b0710a3367c6c2c2e0ed8fd
simplicity15 <dattaskamble1997@gmail.com>
2025-09-06 19:48:46 +0530

    migreation files updated

--- database/migrations/2025_09_04_171523_create_room_details_table.php
+++ database/migrations/2025_09_04_171523_create_room_details_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('room_details', function (Blueprint $table) {
             $table->id('room_detail_id');
+            $table->unsignedBigInteger('room_id');
             $table->string('room_code');
             $table->string('room_number')->nullable();
+            $table->string('check_in_time')->nullable();
+            $table->string('check_out_time')->nullable();
             $table->tinyInteger('current_status')->default(1)->comment('1-Available, 2-Occupied, 3-Maintenance, 4-Under Construction');
             $table->tinyInteger('owner_confirmation_status')->default(1)->comment('1-confirm, 2-cancel');
             $table->timestamps();
